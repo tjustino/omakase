861a41ffb7e388428688169d535ac2e05ee78a50
tjustino <thomas.justino@gmail.com>
2019-10-21 23:13:36 +0200

    je n'affiche que rarement le show

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL : https://guides.rubyonrails.org/routing.html
-  resources :inscriptions
-  resources :encadrants
-  resources :enfants
+  # resources :inscriptions, except: [:show]
+  resources :encadrants, except: [:show]
+  resources :enfants, except: [:show]
 
   get "dice", to: "home#dice"
   root "home#index"
