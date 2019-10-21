1f8a703a2b48892e713876231d4e241865d32a42
tjustino <thomas.justino@gmail.com>
2019-10-21 23:14:09 +0200

    ergonomie

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,19 @@
 
 # Application Helper
 module ApplicationHelper
+  def navbar_item(controller, action)
+    if params[:controller] == controller && params[:action] == action
+      "navbar-item is-active"
+    else
+      "navbar-item"
+    end
+  end
+
+  def navbar_link
+    if ["enfants", "encadrants"].include? params[:controller]
+      "navbar-link is-active"
+    else
+      "navbar-link"
+    end
+  end
 end
