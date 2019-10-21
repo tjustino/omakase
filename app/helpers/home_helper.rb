52c70a0e03337c7336487167ecf0d4c851eabde1
tjustino <thomas.justino@gmail.com>
2019-10-21 22:39:06 +0200

    regroupement des méthodes dans un seul helper

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,4 +1,19 @@
+# frozen_string_literal: true
+
+# Home Helper
 module HomeHelper
+  # GET /
+  # Renvoie une liste d'encadrants
+  def list(encadrants)
+    content_tag(:ul) do
+      encadrants.each do |encadrant|
+        concat(content_tag(:li, encadrant.nom))
+      end
+    end
+  end
+
+  # GET /dice
+  # Renvoie l'encadrant dans le hash de la proposition
   def retrouve_encadrant(jour, enfant, proposition)
     proposition.each do |hash|
       next unless hash[:jour] == jour
