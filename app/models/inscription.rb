00caf716195fbd4f991079f2cf82d1cc50ffda69
tjustino <thomas.justino@gmail.com>
2019-10-23 17:32:20 +0200

    recommandation rubocop

--- app/models/inscription.rb
+++ app/models/inscription.rb
@@ -9,21 +9,24 @@ class Inscription < ApplicationRecord
 
   private ######################################################################
 
-  def self.encadrants(jour, enfant)
-    encadrants = []
-    rows = select(:encadrant_id).where(jour: jour, enfant: enfant.id)
+    class << self
+      def encadrants(jour, enfant, encadrant_ids = [])
+        encadrants = []
+        rows = select(:encadrant_id).where(jour: jour, enfant: enfant.id)
+                                    .where.not(encadrant_id: encadrant_ids)
 
-    rows.each { |row| encadrants << row.encadrant_id }
+        rows.each { |row| encadrants << row.encadrant_id }
 
-    Encadrant.find(encadrants)
-  end
+        Encadrant.find(encadrants)
+      end
 
-  def self.enfants_par_nb_encadrants(jour, enfants_exclus, encadrants_exclus)
-    select(:enfant_id).where(jour: jour)
-                      .where.not(enfant_id: enfants_exclus)
-                      .where.not(encadrant_id: encadrants_exclus)
-                      .group(:enfant_id)
-                      .order('count_enfant_id ASC')
-                      .count
-  end
+      def enfants_par_nb_encadrants(jour, enfants_exclus, encadrants_exclus)
+        select(:enfant_id).where(jour: jour)
+                          .where.not(enfant_id: enfants_exclus)
+                          .where.not(encadrant_id: encadrants_exclus)
+                          .group(:enfant_id)
+                          .order("count_enfant_id ASC")
+                          .count
+      end
+    end
 end
