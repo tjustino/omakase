e34bd98b8998b787303219303a2e2309731a4eb1
tjustino <thomas.justino@gmail.com>
2019-11-13 14:52:16 +0100

    french application

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,17 @@ module Omakase
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # Set Time.zone default to the specified zone and make Active Record
+    # auto-convert to this zone.
+    # Run "rake -D time" for a list of tasks for finding time zone names.
+    # Default is UTC.
+    config.time_zone = "Paris"
+
+    # The default locale is :en and all translations from
+    # config/locales/*.rb,yml are auto loaded.
+    # config.i18n.load_path += \
+    #                   Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :fr
   end
 end
