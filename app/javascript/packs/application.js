9643df86d1dcf2d469498b49443019d66ac9027c
tjustino <thomas.justino@gmail.com>
2019-11-20 16:22:45 +0100

    remove unnecessary requirement

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,6 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start();
-require("channels");
 require('close_button.js');
 
 // Uncomment to copy all static images under ../images to the output folder and reference
